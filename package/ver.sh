3859b6356c164205968aefc56130bd71354fccf3
foobar <turtle.engr@gmail.com>
2022-01-30 22:02:46 -0800

    Packaging

--- package/ver.sh
+++ package/ver.sh
@@ -15,7 +15,7 @@ export ProdName="git-proj"
 export ProdAlias="git-proj"
 # One word [-a-z0-9]
 
-export ProdVer="0.5.3"
+export ProdVer="0.5.4"
 # [0-9]*.[0-9]*{.[0-9]*}
 # Requires 2 numbers, 3'rd number is optional
 # %version ProdVer
