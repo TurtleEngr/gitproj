08764c0b22e25a3303ca732f2d0aabb558638d5e
TurtleEngr <turtle.engr@gmail.com>
2022-02-05 15:45:44 -0800

    Moved scripts to test/utils. Fix clean-git-proj-repo.sh

--- test/util/clean-git-proj-repo.sh
+++ test/util/clean-git-proj-repo.sh
@@ -5,22 +5,25 @@ pMaxNum=${1:-10}
 
 cRelPath=/rel/development/software/own/git-proj/deb
 
-if [ $pMaxNum -le 2 ]; then
-    return 1
+if [ $pMaxNum -le 1 ]; then
+    exit 1
 fi
 if [ ! -d $cRelPath ]; then
-    return 1
+    exit 1
+fi
+if ! cd $cRelPath; then
+    exit 1
 fi
 
-tNumFile=$('ls' $cRelPath | wc -l)
-if [ ${tNumFile:-0} -le $cMaxNum ]; then
-    return 0
+tNumFile=$('ls' git-proj-* | wc -l)
+if [ $tNumFile -le $pMaxNum ]; then
+    exit 0
 fi
 
-(( tNumRm = tNumFile - cMaxNum))
-if [ ${tNumRm:0} -le 0 ]; then
-    return 1
+(( tNumRm = tNumFile - pMaxNum ))
+if [ ${tNumRm:-0} -le 0 ]; then
+    exit 1
 fi
 
-rm -v $('ls' -tr | head -n $tNumRm)
-return $?
+rm -vf $('ls' -tr git-proj-* | head -n $tNumRm)
+exit $?
