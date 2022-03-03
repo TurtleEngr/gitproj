f48a44c9c3bb34bffa85dd9735da4d4c1f7fa703
TurtleEngr <turtle.engr@gmail.com>
2022-03-02 22:01:16 -0800

    Issue #51 is probably fixed.

--- package/ver.sh
+++ package/ver.sh
@@ -15,7 +15,7 @@ export ProdName="git-proj"
 export ProdAlias="git-proj"
 # One word [-a-z0-9]
 
-export ProdVer="0.7.2"
+export ProdVer="0.7.3"
 # [0-9]*.[0-9]*{.[0-9]*}
 # Requires 2 numbers, 3'rd number is optional
 # First, Major changes:
