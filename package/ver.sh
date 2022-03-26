bc22bff6d1d81f5739ef866ed41ed87f2b1da067
TurtleEngr <turtle.engr@gmail.com>
2022-03-26 12:17:30 -0700

    Inc version. Getting ready for more changes.

--- package/ver.sh
+++ package/ver.sh
@@ -15,7 +15,7 @@ export ProdName="git-proj"
 export ProdAlias="git-proj"
 # One word [-a-z0-9]
 
-export ProdVer="0.7.6"
+export ProdVer="0.7.7"
 # [0-9]*.[0-9]*{.[0-9]*}
 # Requires 2 numbers, 3'rd number is optional
 # First, Major changes:
